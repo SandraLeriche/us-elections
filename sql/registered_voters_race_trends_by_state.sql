0ceb0ae86677a33bfc32c9d34bd7cc08d52ccff9
Sandra Leriche <sandra.m.leriche@student.uts.edu.au>
2020-10-14 21:49:00 +1100

    creating tables on registered voters by state and race
    output table from sql query

---
+++ sql/registered_voters_race_trends_by_state.sql
@@ -0,0 +1,5 @@
+SELECT state_name as state, AVG(white_perc), AVG(black_perc), AVG(asia_perc), AVG(native_american_perc), AVG(native_hawaiian_perc), AVG(hispanic_perc), AVG(white_non_hispanic_perc), AVG(multi_race_perc), registered_per_total
+FROM registered_voters_race_hispanic_origin r
+LEFT JOIN registered_voters as v
+ON v.state = r.state_name
+GROUP BY state_name;
